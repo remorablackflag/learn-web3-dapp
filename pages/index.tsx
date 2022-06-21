daf97d6aa138738e8ebdc9ac449a71888ac4c06f
Remora Blackflag <remorablackflag@users.noreply.github.com>
2022-06-21 15:23:48 +0200

    hello world

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,8 @@ export default function Index() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Home />
+      <body>Hello World!</body>
+      {/* <Home /> */}
     </>
   );
 }
